e9ff9e81bf743472f3964e5d0a0d0fa2454420bf
rkorostin <rkorostin@gmail.com>
2022-10-01 15:29:29 +0300

    Задание №6 по семинару от 30.09.2022

---
+++ 265741/Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// На входе - число, на выходе выдаёт является ли число чётным 
+// (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int num = int.Parse(userInput);
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число " + num + " чётное");
+}
+else
+{
+    Console.WriteLine("Число " + num + " нечётное"); 
+}
