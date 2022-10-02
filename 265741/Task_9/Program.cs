ae70b569295067133f870f85c88e2ddb7fa51bd5
rkorostin <rkorostin@gmail.com>
2022-10-02 10:09:35 +0300

    Доп. задание по семинару от 30.09.2022

---
+++ 265741/Task_9/Program.cs
@@ -0,0 +1,14 @@
+﻿// На входе число (N), а на выходе показывает все нечётные числа от 1 до N.
+// Например, 8 -> 1, 3, 5, 7
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int num = int.Parse(userInput);
+int count = -1;
+
+Console.Write(num + " -> ");
+while (count < num - 1)
+{
+    count += 2;
+    Console.Write(count + " ");
+}
+
