712cf96dddf0234048b7e109b78a1a456546a7c5
rkorostin <rkorostin@gmail.com>
2022-10-03 15:37:48 +0300

    Task_001 Поиск максимального элемента в массиве

---
+++ Additional/Task_001_MaxEltArray/Program.cs
@@ -0,0 +1,20 @@
+﻿// Поиск кол-ва максимальных элементов в массиве
+int[] array = { 1, 21, 23, 45, 66, 66, 47, 28, 19 };
+int index = 0;
+int max = array[index];
+int countMax = 0;
+
+while (index < array.Length)
+{
+    if (array[index] > max)
+    {
+        max = array[index];
+        countMax = 1;
+    }
+    else if (array[index] == max)
+    {
+        countMax++;
+    }
+    index++;
+}
+Console.WriteLine(countMax);
