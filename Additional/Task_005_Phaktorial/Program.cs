dd01cbcb1be376ddc5c5a689df79f2b3cb3d0036
rkorostin <rkorostin@gmail.com>
2022-10-04 09:57:39 +0300

    Task_005 Поиск факториала числа N

---
+++ Additional/Task_005_Phaktorial/Program.cs
@@ -0,0 +1,16 @@
+﻿// Поиск факториала введенного числа N
+
+Console.WriteLine("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int num = int.Parse(userInput);
+
+int N = 1;
+int index = 1;
+
+while (index <= num)
+{
+    N = N * index;
+    index++;
+}
+
+Console.WriteLine("Факториал числа " + num + " -> " + N);
