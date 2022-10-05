08884547ae7b249926e8f8ca1780aeea5077c1a1
rkorostin <rkorostin@gmail.com>
2022-10-05 21:15:06 +0300

    Задание 15. Семинар от 04.10.2022

---
+++ 265742/Task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.Write("Введите любое целое число от 1 до 7: ");
+int Number;
+while (true)
+{
+    if (int.TryParse(Console.ReadLine(), out Number) && Number <= 7 && Number >= 1)
+        break;
+    Console.WriteLine($"Ошибка ввода! Попробуйте снова ввести любое число от 1 до 7");
+}
+
+if (Number == 1) Console.WriteLine($"{Number} -> Нет");
+else if (Number == 2) Console.WriteLine($"{Number} -> Нет");
+else if (Number == 3) Console.WriteLine($"{Number} -> Нет");
+else if (Number == 4) Console.WriteLine($"{Number} -> Нет");
+else if (Number == 5) Console.WriteLine($"{Number} -> Нет");
+else if (Number == 6) Console.WriteLine($"{Number} -> Да");
+else if (Number == 7) Console.WriteLine($"{Number} -> Да"); 
