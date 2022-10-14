fc2e0eb73e25a05365a01ff025f309875c688250
rkorostin <rkorostin@gmail.com>
2022-10-14 22:27:57 +0300

    Задача 25. Ввод числа А и В. Затем возведение числа А в степень В. Поправлен второй метод

--- 265744/Task_25/Program.cs
+++ 265744/Task_25/Program.cs
@@ -6,53 +6,63 @@
 // Из семинара - задачу надо решить через отдельный метод, который возводит число 
 // в степень. Т.е. использовать математическую функцию Math.Pow нельзя!
 
-// -------------------------- Начало программы -------------------------------------
-Console.Clear();
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // -------------------------- Начало программы -------------------------------------
+        Console.Clear();
 
-int number = GetNumberByUser("Введите число: ", "Ошибка! Нужно ввести любое целое число!");
-int degree = GetDegreeByUser("Введите степень: ", "Ошибка! Нужно ввести натуральное число!");
-Elevation(number, degree);
-// -------------------------- Конец программы --------------------------------------
+        int number = GetNumberByUser("Введите число: ", "Ошибка! Нужно ввести любое целое число!");
+        int degree = GetDegreeByUser("Введите степень: ", "Ошибка! Нужно ввести натуральное число!");
+        
+        Elevation(number, degree);
+        // -------------------------- Конец программы --------------------------------------
 
-// -------------------------- Определение методов ----------------------------------
-// Метод GetNumberByUser запрашивает число, которое надо возвести в степень.
-// Число может быть и отрицательным и положительным, но целым!
-int GetNumberByUser(string message, string errorMessage)
-{
-    while (true)
-    {
-        Console.Write(message); // вывод в консоль сообщения
-        bool isCorrect = int.TryParse(Console.ReadLine(), out int num); // преобразовывает введённую пользователем строку в число типа int.       
-        if (isCorrect)
-            return num;
-        Console.WriteLine(errorMessage); // В случае ошибки выводит в консоль сообщение errorMessage
+        // -------------------------- Определение методов ----------------------------------
+        // Метод GetNumberByUser запрашивает число, которое надо возвести в степень.
+        // Число может быть и отрицательным и положительным, но целым!
+        int GetNumberByUser(string message, string errorMessage)
+        {
+            while (true)
+            {
+                Console.Write(message); // вывод в консоль сообщения
+                bool isCorrect = int.TryParse(Console.ReadLine(), out int num); // преобразовывает введённую пользователем строку в число типа int.       
+                if (isCorrect)
+                    return num;
+                Console.WriteLine(errorMessage);
+            }
+        }
+
+        // Метод GetDegreeByUser запрашивает степень, в которую надо возводить число.
+        // Число может быть только положительным.
+        int GetDegreeByUser(string message, string errorMessage)
+        {
+            while (true)
+            {
+                int userInput = GetNumberByUser(message, errorMessage);
+                if (userInput > 0)
+                {
+                    return userInput;
+                }
+                Console.WriteLine(errorMessage);
+            }
+        }
+
+        /* Создание метода Elevation для выполнения действий над введёнными пользователем
+        данными. В теле метода Elevation используется цикл for для 
+        возведения числа в степень.
+        */
+        int Elevation(int number, int degree)
+        {
+            int numberInDegree = 1;
+            for (int i = 0; i < degree; i++)
+            {
+                numberInDegree *= number;
+            }
+            Console.WriteLine($"{number} ^ {degree} = {numberInDegree}");
+            return numberInDegree;
+        }
     }
-}
-// Метод GetDegreeByUser запрашивает степень, в которую надо возводить число.
-// Число может быть и отрицательным и положительным.
-int GetDegreeByUser(string message, string errorMessage)
-{
-    while (true)
-    {
-        Console.Write(message);
-        bool isCorrect = int.TryParse(Console.ReadLine(), out int num) && num > 0;
-        if (isCorrect)
-            return num;
-        Console.WriteLine(errorMessage);
-    }
-}
-/* Создание метода Elevation для выполнения действий над введёнными пользователем
-данными. В теле метода Elevation используется цикл for для 
-возведения числа в степень.
-*/
-int Elevation(int number, int degree)
-{
-    int numberInDegree = 1;
-    for (int i = 0; i < degree; i++)
-    {
-        numberInDegree *= number;
-    }
-    Console.WriteLine($"{number} ^ {degree} = {numberInDegree}");
-    return numberInDegree;
 }
 // -------------------------- Конец определения методов ----------------------------
