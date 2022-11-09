0489f5449c6431bbccf4de05337d61f92a5acb92
rkorostin <rkorostin@gmail.com>
2022-11-09 23:13:42 +0300

    Задача 54. Сортированный массив по строкам (убывание)

---
+++ 265748/Task_54/Program.cs
@@ -0,0 +1,68 @@
+﻿/* Задача 54: Задайте двумерный массив. 
+Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+Console.Clear();
+
+Console.WriteLine($"Исходный массив:");
+int[,] array = FillArray(3, 4, 10);
+PrintArray(array);
+
+Console.WriteLine($"\nОтсортированный массив:");
+SortArray(array);
+PrintArray(array);
+
+//Наполняем массив случайными числами
+int[,] FillArray(int y, int x, int value)
+{
+    int[,] result = new int[y, x];
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            result[i, j] = new Random().Next(0, value + 1);
+        }
+    }
+    return result;
+}
+
+//Выводим на печать
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//Сортируем элементы в строках по убыванию
+void SortArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1) - 1; j++)
+        {
+            for (int k = j + 1; k < inArray.GetLength(1); k++)
+            {
+                if (inArray[i, k] > inArray[i, j])
+                {
+                    int max = inArray[i, k];
+                    inArray[i, k] = inArray[i, j];
+                    inArray[i, j] = max;
+                }
+            }
+        }
+    }
+}
