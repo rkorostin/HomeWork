3b8663048e608cbfda6e3fa17277656ef5318c2b
rkorostin <rkorostin@gmail.com>
2022-11-11 08:39:10 +0300

    Задача 68. Рекурсия. Аккерман

--- 265749/Task_68/Program.cs
+++ 265749/Task_68/Program.cs
@@ -3,6 +3,7 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 
+ [contributor guide index](https://github.com/Azure/azure-content/blob/master/contributor-guide/contributor-guide-index.md).
 https://ru.wikipedia.org/wiki/%D0%A4%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8F_%D0%90%D0%BA%D0%BA%D0%B5%D1%80%D0%BC%D0%B0%D0%BD%D0%B0
        Формула                             Условие
         | n + 1,                         m = 0;
